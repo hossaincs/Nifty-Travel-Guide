32f00be65015d9b13af2c8bb0dc32d22896fa54a
mdyousufh <yousufhossan2015@gmail.com>
2021-11-13 23:43:21 +0600

    Design

---
+++ app/src/main/java/com/cityUniversity/niftytravelguide/Driver.java
@@ -0,0 +1,13 @@
+package com.cityUniversity.niftytravelguide;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Driver extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_driver);
+    }
+}
---
+++ app/src/main/java/com/cityUniversity/niftytravelguide/Tourist.java
@@ -0,0 +1,13 @@
+package com.cityUniversity.niftytravelguide;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Tourist extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tourist);
+    }
+}
