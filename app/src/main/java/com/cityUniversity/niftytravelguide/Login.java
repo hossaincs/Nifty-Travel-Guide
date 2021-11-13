95de32a7dddd934c1c1c22c4768ba461681136d1
mdyousufh <yousufhossan2015@gmail.com>
2021-11-14 00:35:01 +0600

    code

--- app/src/main/java/com/cityUniversity/niftytravelguide/Login.java
+++ app/src/main/java/com/cityUniversity/niftytravelguide/Login.java
@@ -1,13 +1,161 @@
 package com.cityUniversity.niftytravelguide;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.Objects;
 
 public class Login extends AppCompatActivity {
+
+    EditText email,password;
+    TextView forgotTextLink;
+    Button loginBtn,gotoRegister;
+    boolean valid = true;
+
+    FirebaseAuth fAuth;
+    FirebaseFirestore fStore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        fAuth = FirebaseAuth.getInstance ();
+        fStore = FirebaseFirestore.getInstance ();
+
+        email = findViewById(R.id.loginEmailId);
+        password = findViewById(R.id.loginPasswordId);
+        loginBtn = findViewById(R.id.loginBtnId);
+        gotoRegister = findViewById(R.id.gotoRegisterId);
+        forgotTextLink = findViewById(R.id.forgetPasswordId);
+
+        loginBtn.setOnClickListener (v -> {
+            checkField ( email );
+            checkField ( password );
+
+            if(valid){
+
+                fAuth.signInWithEmailAndPassword ( email.getText ().toString (),password.getText ().toString () ).addOnSuccessListener (authResult -> {
+                    Toast.makeText ( Login.this , "Logged in Successfully" , Toast.LENGTH_SHORT ).show ( );
+                    checkUserAccessLevel ( Objects.requireNonNull ( authResult.getUser ( ) ).getUid () );
+                }).addOnFailureListener (e -> Toast.makeText ( Login.this , "Login Failed" , Toast.LENGTH_SHORT ).show ( ));
+
+
+
+            }
+        });
+
+
+
+
+        gotoRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Login.this.startActivity(new Intent(Login.this.getApplicationContext(), Register.class));
+            }
+        });
+
+        forgotTextLink.setOnClickListener(v -> {
+
+            EditText resetMail = new EditText(v.getContext());
+            AlertDialog.Builder passwordResetDialog = new AlertDialog.Builder(v.getContext());
+            passwordResetDialog.setTitle("Reset Password ?");
+            passwordResetDialog.setMessage("Enter Your Email To Received Reset Link");
+            passwordResetDialog.setView(resetMail);
+
+            passwordResetDialog.setPositiveButton("Yes", (dialog, which) -> {
+
+                String mail = resetMail.getText().toString();
+                fAuth.sendPasswordResetEmail(mail).addOnSuccessListener(unused -> Toast.makeText(Login.this, "Reset Link Sent to Your Email", Toast.LENGTH_SHORT).show()).addOnFailureListener(e -> Toast.makeText(Login.this, "Error ! Reset Link is Not Sent" +e.getMessage(), Toast.LENGTH_SHORT).show());
+            });
+
+            passwordResetDialog.setNegativeButton("No", (dialog, which) -> {
+
+            });
+
+            passwordResetDialog.create().show();
+
+
+
+        });
+
+
+    }
+
+    private void checkUserAccessLevel (String uid) {
+
+        DocumentReference df = fStore.collection ( "Users").document(uid);
+        df.get ().addOnSuccessListener (documentSnapshot -> {
+
+            Log.d ( "TAG","onSuccess: "+documentSnapshot.getData ());
+
+            if(documentSnapshot.getString ( "isTouristId" )!=null){
+
+                startActivity ( new Intent ( getApplicationContext (), Tourist.class ) );
+                finish ();
+            }
+            if(documentSnapshot.getString ( "isAdminId" )!=null) {
+
+                startActivity ( new Intent ( getApplicationContext ( ) , Admin.class ) );
+                finish ( );
+            }
+            if(documentSnapshot.getString ( "isDriverId" )!=null) {
+
+                startActivity ( new Intent ( getApplicationContext ( ) , Driver.class ) );
+                finish ( );
+            }
+        });
+    }
+
+    public void checkField(EditText textField){
+        if(textField.getText().toString().isEmpty()){
+            textField.setError("Error");
+            valid = false;
+        }else {
+            valid = true;
+        }
+
+    }
+
+    @Override
+    protected void onStart () {
+        super.onStart ( );
+
+        if(FirebaseAuth.getInstance ().getCurrentUser ()!=null){
+
+            DocumentReference df = FirebaseFirestore.getInstance ().collection ( "Users").document (FirebaseAuth.getInstance ().getCurrentUser ().getUid ());
+            df.get ().addOnSuccessListener (documentSnapshot -> {
+                if(documentSnapshot.getString ( "isAdminId") !=null){
+                    startActivity ( new Intent ( getApplicationContext ( ) , Admin.class ) );
+                    finish ( );
+
+                }
+                if(documentSnapshot.getString ( "isPassengerId") !=null) {
+                    startActivity ( new Intent ( getApplicationContext ( ) , Tourist.class ) );
+                    finish ( );
+                }
+                if(documentSnapshot.getString ( "isDriverId") !=null) {
+                    startActivity ( new Intent ( getApplicationContext ( ) , Driver.class ) );
+                    finish ( );
+                }
+            }).addOnFailureListener (e -> {
+                FirebaseAuth.getInstance ().signOut ();
+                startActivity ( new Intent ( getApplicationContext (),Login.class ));
+                finish ();
+            });
+        }
     }
 }
--- app/src/main/java/com/cityUniversity/niftytravelguide/Register.java
+++ app/src/main/java/com/cityUniversity/niftytravelguide/Register.java
@@ -1,13 +1,205 @@
 package com.cityUniversity.niftytravelguide;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class Register extends AppCompatActivity {
+
+    EditText name,email,password,phone;
+    CheckBox driver,tourist,admin;
+    TextView login;
+    boolean valid = true;
+    Button create;
+
+    FirebaseAuth fAuth;
+
+    FirebaseFirestore fStore;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        fAuth = FirebaseAuth.getInstance();
+        fStore = FirebaseFirestore.getInstance();
+        name = findViewById(R.id.registerNameId);
+        email = findViewById(R.id.registerEmailId);
+        password = findViewById(R.id.registerPasswordId);
+        phone = findViewById(R.id.registerPhoneNumberId);
+        login = findViewById(R.id.gotoLoginId);
+        create = findViewById(R.id.registerBtnId);
+        admin = findViewById ( R.id.isAdminId );
+        driver = findViewById ( R.id.isDriverId );
+        tourist = findViewById ( R.id.isTouristId);
+
+        //check boxes logics
+
+        admin.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if(compoundButton.isChecked()){
+
+                    driver.setChecked(false);
+                    tourist.setChecked(false);
+
+                }
+            }
+        });
+
+        tourist.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if(compoundButton.isChecked()){
+
+                    driver.setChecked(false);
+                    admin.setChecked(false);
+
+                }
+            }
+        });
+
+        driver.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if(compoundButton.isChecked()){
+
+                    tourist.setChecked(false);
+                    admin.setChecked(false);
+
+                }
+            }
+        });
+
+
+        create.setOnClickListener(view -> {
+            checkField(name);
+            checkField(email);
+            checkField(password);
+            checkField(phone);
+            admin = findViewById ( R.id.isAdminId );
+            tourist = findViewById ( R.id.isTouristId );
+            driver = findViewById ( R.id.isDriverId );
+
+            //checkbox validation
+
+
+
+
+
+            if(valid)
+            {
+                fAuth.createUserWithEmailAndPassword(email.getText().toString(),password.getText().toString()).addOnSuccessListener(authResult -> {
+                    FirebaseUser user = fAuth.getCurrentUser();
+                    String uid=user.getUid();
+
+                    Toast.makeText(Register.this, "Successfully Account Created", Toast.LENGTH_SHORT).show();
+
+                    assert user != null;
+                    DocumentReference df =fStore.collection("Users").document(user.getUid());
+
+                    Map<String,Object> userInfo = new HashMap<>();
+                    userInfo.put("FullName",name.getText().toString());
+                    userInfo.put("UserEmail",email.getText().toString());
+                    userInfo.put("PhoneNumber",phone.getText().toString());
+
+                    //specify if user is admin
+
+                    if(admin.isChecked ()) {
+
+                        userInfo.put ( "isAdminId", "1");
+
+                        Map<String,Object> admin = new HashMap<>();
+                        admin.put("FullName",name.getText().toString());
+                        admin.put("UserEmail",email.getText().toString());
+                        admin.put("PhoneNumber",phone.getText().toString());
+                        admin.put("uid",uid);
+                        admin.put ( "type", "admin");
+                        FirebaseDatabase database=FirebaseDatabase.getInstance();
+                        DatabaseReference reference=database.getReference("admin");
+
+
+
+                        reference.child(uid).setValue(admin);
+                    }
+
+                    if(tourist.isChecked ()){
+
+                        userInfo.put ( "isTouristId","1" );
+
+                        Map<String,Object> tourist = new HashMap<>();
+                        tourist.put("FullName",name.getText().toString());
+                        tourist.put("UserEmail",email.getText().toString());
+                        tourist.put("PhoneNumber",phone.getText().toString());
+                        tourist.put("uid",uid);
+                        tourist.put ( "type", "tourist");
+                        FirebaseDatabase database=FirebaseDatabase.getInstance();
+                        DatabaseReference reference=database.getReference("tourist");
+
+                        reference.child(uid).setValue(tourist);
+                    }
+
+                    if(driver.isChecked ()){
+
+                        userInfo.put ( "isDriverId","1" );
+
+                        Map<String,Object> driver = new HashMap<>();
+                        driver.put("FullName",name.getText().toString());
+                        driver.put("UserEmail",email.getText().toString());
+                        driver.put("PhoneNumber",phone.getText().toString());
+                        driver.put("uid",uid);
+                        driver.put ( "type", "driver");
+                        FirebaseDatabase database=FirebaseDatabase.getInstance();
+                        DatabaseReference reference=database.getReference("driver");
+
+                        reference.child(uid).setValue(driver);
+                    }
+
+
+                    df.set(userInfo);
+                    startActivity(new Intent(getApplicationContext(), Login.class));
+                    finish();
+                }) .addOnFailureListener(e -> Toast.makeText(Register.this, "Failed to Created Account", Toast.LENGTH_SHORT).show());
+
+
+
+            }
+
+
+
+        });
+
+
+        login.setOnClickListener (view -> startActivity ( new Intent ( getApplicationContext ( ) , Login.class ) ));
+
+
     }
+    public void checkField(EditText textField){
+        if(textField.getText().toString().isEmpty()){
+            textField.setError("Error");
+            valid = false;
+        }else {
+            valid = true;
+        }
+
+    }
+
 }
